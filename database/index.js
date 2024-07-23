3953d03180e4d5ae950f76031c80a508399c3c3f
thomas <thomasfong102@gmail.com>
2024-07-23 21:55:38 +0800

    update inventoryRoute.js

--- database/index.js
+++ database/index.js
@@ -35,6 +35,3 @@ module.exports = {
   })
   module.exports = pool
 }
-
-
-    
--- models/inventory-model.js
+++ models/inventory-model.js
@@ -1,4 +1,4 @@
-const pool = require("../database/index")
+const pool = require("../database/")
 
 /* ***************************
  *  Get all classification data
